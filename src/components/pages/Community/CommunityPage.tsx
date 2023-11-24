f8c96b5a04f53c5a1f988fae928a14e108edf57a
Maykon Sousa <maykon.sousa@hotmail.com>
2023-11-24 16:13:42 -0300

    feat: add filter on the community page

--- src/components/pages/Community/CommunityPage.tsx
+++ src/components/pages/Community/CommunityPage.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import React from 'react';
 import { useGetProfiles } from '@/hooks';
 import { Loading } from '@/components/Loading';
 import { EmptyState } from '@/components/EmptyState';
+import { Search } from '@mui/icons-material';
+import { FormControl, TextField, InputAdornment } from '@mui/material';
 import { CardsContainer, PageContainer, Title } from './CommunityPage.styles';
 import { UserCard } from './fragments';
 
@@ -32,8 +35,20 @@ interface IUserData {
 
 export function CommunityPage() {
   const { data, loading, error } = useGetProfiles();
+  const [searchText, setSearchText] = React.useState<string>('');
+  const [filteredUsers, setFilteredUsers] = React.useState<IUserData[]>([]);
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
 
-  const users: IUserData[] = data || [];
+  React.useEffect(() => {
+    if (data as IUserData[]) {
+      const filtered = data.filter((user: IUserData) =>
+        user.name.toLowerCase().includes(searchText.toLowerCase()),
+      );
+      setFilteredUsers(filtered);
+    }
+  }, [searchText, data]);
 
   return (
     <PageContainer>
@@ -47,13 +62,37 @@ export function CommunityPage() {
       {loading ? (
         <Loading />
       ) : (
-        <CardsContainer>
-          {users.length ? (
-            users.map((user) => <UserCard key={user.id} user={user} />)
-          ) : (
-            <EmptyState message="Nenhum usuário encontrado." type="empty" />
-          )}
-        </CardsContainer>
+        <>
+          <FormControl
+            fullWidth
+            sx={{
+              marginBottom: '20px',
+            }}
+          >
+            <TextField
+              label="Pesquisar"
+              fullWidth
+              value={searchText}
+              onChange={handleSearch}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Search />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </FormControl>
+          <CardsContainer>
+            {filteredUsers.length ? (
+              filteredUsers.map((user) => (
+                <UserCard key={user.id} user={user} />
+              ))
+            ) : (
+              <EmptyState message="Nenhum usuário encontrado." type="empty" />
+            )}
+          </CardsContainer>
+        </>
       )}
     </PageContainer>
   );
