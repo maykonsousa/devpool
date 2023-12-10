e03f6be7d4c0076d273d7941f2050de767b6afb1
Maykon Sousa <maykon.sousa@hotmail.com>
2023-12-09 23:24:20 -0300

    Delete src/validations directory

--- src/validations/formValidations/languagesValidation.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const languagesValidation = z.object({
-  name: z.string().min(1, { message: 'Campo obrigatório' }),
-  level: z.string().min(1, { message: 'Campo obrigatório' }),
-});
--- src/validations/formValidations/loginFormValidations.ts
+++
@@ -1,6 +0,0 @@
-import z from 'zod';
-
-export const loginFormValidations = z.object({
-  username: z.string().min(1, { message: 'Campo obrigatório' }),
-  password: z.string().min(1, { message: 'Campo obrigatório' }),
-});
