731094c9b969e31ff871636ddea7874b9c6a94d8
Maykon Sousa <maykon.sousa@hotmail.com>
2023-12-09 23:13:31 -0300

    fix: fixes import

--- src/validations/formValidations/index.ts
+++ src/validations/formValidations/index.ts
@@ -1,10 +1,11 @@
+export { createCourseValidation } from './coursesValidations';
+export { emailValidation } from './emailValidations';
+export { createFeedbackValidation } from './feedbackValidations';
 export { createJobValidation } from './jobsValidations';
+export { languagesValidation } from './languagesValidation';
 export { loginFormValidations } from './loginFormValidations';
+export { createProjectValidation } from './projectsValidations';
 export {
   creteDeveloperValidation,
   updateDeveloperValidation,
 } from './userFormValidation';
-export { createProjectValidation } from './projectsValidations';
-export { createCourseValidation } from './coursesValidations';
-export { createFeedbackValidation } from './feedbackValidations';
-export { languagesValidation } from './languagesValidation';
