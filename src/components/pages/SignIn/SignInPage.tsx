388cbebe3dd3343e9aeb425a612efbd7ddc61141
Maykon Sousa <maykon.sousa@hotmail.com>
2024-01-06 12:45:22 -0300

    fix: shoow feedback error login

--- src/components/pages/SignIn/SignInPage.tsx
+++ src/components/pages/SignIn/SignInPage.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import Image from 'next/image';
 import { FormProvider, useForm, SubmitHandler } from 'react-hook-form';
 import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
@@ -12,9 +12,9 @@ import { PassInput } from '@/components/PassInput';
 
 import { Loading } from '@/components/Loading';
 import { LoginButton } from '@/components/LoginButton';
-import { useLogin } from '@/hooks';
+import { useFeedback, useLogin } from '@/hooks';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { loginFormValidations } from '@/validations/loginFormValidations';
 import { SignInFormContainer, SignInPageContainer } from './SignIn.styles';
 
@@ -30,23 +30,41 @@ export function SignInPage() {
     handleLinkedinLogin,
     loading,
   } = useLogin();
+  const params = useSearchParams();
+  const { showMessage } = useFeedback();
+
   const theme = useTheme();
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
   const methods = useForm<IFormData>({
     resolver: zodResolver(loginFormValidations),
   });
   const router = useRouter();
+  const errorLogin = params.get('error');
 
   const submitWithCredentials: SubmitHandler<IFormData> = async (data) => {
     await handleCredentialsLogin(data);
   };
+
+  const validateError = useCallback(() => {
+    if (errorLogin === 'CredentialsSignin' && status === 'unauthenticated') {
+      showMessage({
+        message: 'Falha ao fazer login. Verifique suas credenciais.',
+        type: 'error',
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [errorLogin, status]);
   const handleSubmit = methods.handleSubmit(submitWithCredentials);
   useEffect(() => {
     if (session) {
       router.push('/community');
     }
   }, [session, router]);
+
+  useEffect(() => {
+    validateError();
+  }, [validateError]);
   return (
     <SignInPageContainer>
       <SignInFormContainer>
